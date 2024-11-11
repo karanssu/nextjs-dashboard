db5b0d73f21b0aa2ee232ed39cccabf2090c07b6
karanssu <s0377009salemstate@gmail.com>
2024-11-10 19:32:56 -0500

    Suspense streaming added for cards in dashboard

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,3 +1,4 @@
+import CardWrapper from "@/app/ui/dashboard/cards";
 import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
@@ -5,6 +6,7 @@ import { lusitana } from "@/app/ui/fonts";
 import { fetchCardData } from "@/app/lib/data";
 import { Suspense } from "react";
 import {
+  CardsSkeleton,
   LatestInvoicesSkeleton,
   RevenueChartSkeleton,
 } from "@/app/ui/skeletons";
@@ -23,14 +25,9 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="collected" value={totalPaidInvoices} type="collected" />
-        <Card title="pending" value={totalPendingInvoices} type="pending" />
-        <Card title="total invoices" value={numberOfInvoices} type="invoices" />
-        <Card
-          title="total customers"
-          value={numberOfCustomers}
-          type="customers"
-        />
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
